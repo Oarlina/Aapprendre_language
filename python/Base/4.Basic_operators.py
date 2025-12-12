08137e4a8bb3898348b698c85a34e8f7be9fe082
Rachel Marquant <marquantrachel@gmail.com>
2025-12-12 15:37:53 +0100

    Ajout de la page basic Operators

---
+++ python/Base/4.Basic_operators.py
@@ -0,0 +1,47 @@
+#-------------------opérateurs arithmétiques-------------------
+
+# Adition
+addition = 2 + 5
+# Soustraction
+subtraction = 50 - 6
+# Multiplication
+multiplication = 5 * 5
+# Division (donne un float)
+division = 92 / 3
+# Reste de division (donne un entier)
+modulo = 92 % 3
+# calculé au carré
+squared = 7 ** 2
+# calculé au cube
+cubed = 2 ** 3
+# calculé au n (ici vaut 15)
+ned = 3 ** 15
+
+
+print ("2 + 5 =", addition, "is true ?", 2 + 5)
+print ("50 - 6 =", subtraction, "is true ?",50 - 6)
+print ("5 * 5 =", multiplication, "is true ?", 5 * 5)
+print ("92 / 3 =", division, "is true ?", 92 / 3)
+print ("92 % 3 =", modulo, "is true ?", 92 % 3)
+print ("7 ** 2 =", squared, "is true ?", 7 ** 2)
+print ("2 ** 3 =", cubed, "is true ?", 2 ** 3)
+print ("3 ** 15 =", ned, "is true ?", 3 ** 15)
+
+#-------------------opérations avec des chaines de caractères-------------------
+
+bonjour_chat = "Bonjour" + " " + "le" + " " + "chat"
+print (bonjour_chat)
+
+tenByte_ = "Bytes_S" * 10 
+print (tenByte_)
+
+#-------------------opérations avec des chaines de caractères-------------------
+
+nbPair = [2,4,6,8,10,12,14,16,18,20]
+nbImpair = [1,3,5,7,9,11,13,15,17,19]
+
+vingtNb = nbPair + nbImpair
+print (vingtNb)
+
+print ([3,6,9] * 3)
+
