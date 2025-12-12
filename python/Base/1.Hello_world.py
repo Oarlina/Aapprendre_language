d44b05fd8f19e347acb4aa79aefcb3b0d82edc34
Rachel Marquant <marquantrachel@gmail.com>
2025-12-12 14:31:00 +0100

    Ajout de la page hello world

---
+++ python/Base/1.Hello_world.py
@@ -0,0 +1,12 @@
+print("Hello world")
+#Hello world
+print ("The comma is like", "a space")
+#The comma is like a space
+print( "The string anti-slash n make a return \n like that")
+#The sting anti-slash n make a return
+#like that
+print (10 * 5)
+#50
+print( 10 + "chat")
+# erreur 
+
