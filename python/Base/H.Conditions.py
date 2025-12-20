94d696614c544cde762cb55191adda14fe680319
Rachel Marquant <marquantrachel@gmail.com>
2025-12-20 14:54:48 +0100

    Ajout de la page Conditions

--- python/Base/H.Conditions.py
+++ python/Base/H.Conditions.py
@@ -1,8 +1,30 @@
  #boucle de si ... et si ... et
-x = 6
-if x == 6 : #== permet de tester un égalité
-    print ("x vaut 6")
-elif type(x) == type(int()) : # elif permet de vérifier une égalité qui n'est pas la première
-    print ("x est un entier")
+age = 6
+name = "Max"
+if age == 6 : #== permet de tester un égalité
+    print ("age vaut 6")
+elif type(age) == type(int()) : # elif permet de vérifier une égalité qui n'est pas la première
+    print ("age est un entier")
 else : # else est choisi si aucune des réponse précedente n'était vraie
-    print ("x n'est pas 6, ni un entier")
+    print ("age n'est pas 6, ni un entier")
+
+# boucle de if réduite
+print ("'%s' vaut t'il 2 ? %s" % (age, age==2))
+
+# boucle de if ou on vérifie que age est un entier valant 6, et si age est l'un des deuage
+if age == 6 and type(6)==type(int()):
+    print ('age est un entier valant 6')
+if age == 6 or type(6)==type(int()):
+    print ('age est soit un entier soit il vaut 6')
+
+# on vérifie si le nom est dans la liste
+if name in ["Charles", "Max", "Arthur", "Rick"]:
+    print ("Le prenom est soit : Charles, Max, Arthur, Rick")
+else:
+    print ("Le prenom n'est pas : Charles, Max, Arthur, Rick")
+
+# on peux remplacer '==' par 'is'
+print (age is name)
+
+# not() inverse l'expression
+print ("'not(True)' vaut %s" %(not(True)))
